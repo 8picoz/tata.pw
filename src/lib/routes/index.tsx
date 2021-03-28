ea280f9395a1d6ce1a332bf71faa35a8e013ba33
たた <8picomu@gmail.com>
2021-03-29 05:53:49 +0900

    [add] routes

---
+++ src/lib/routes/index.tsx
@@ -0,0 +1,10 @@
+import { route, mount } from "navi";
+import React from "react";
+import Top from "../../components/pages/Top/Top"
+
+export const routes = mount({
+    "/": route({
+        title: "tata.pw",
+        view: <Top />,
+    }),
+});
